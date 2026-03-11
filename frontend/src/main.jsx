282370f6ccf4159a2ad06b8db016d3b01d2daa05
Vivek Meena <vivekmeena2004vm@gmail.com>
2026-03-11 11:18:02 +0530

    Updated git rebase --continue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -13,5 +12,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </UserContext>
     </AuthContext>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
