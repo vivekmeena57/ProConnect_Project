decff027c1921a0f8fc0badfeca0e24a174fe05b
Vivek Meena <vivekpatelmeena893@gmail.com>
2026-03-11 12:38:47 +0530

    Update CORS configuration to allow additional origin

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,10 @@ let server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+   origin: [
+    "http://localhost:5173",
+    "https://proconnect-frontend-ochre.vercel.app"
+  ],
     credentials: true,
   },
 });
@@ -27,7 +30,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://proconnect-frontend-ochre.vercel.app"
+  ],
     credentials: true,
   }),
 );
